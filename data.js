f3e52ddb07914a91e920bb4760a9c65c0116023e
Bendouba Abdessalem <moi.cest.salamo@gmail.com>
2025-11-04 02:36:22 +0100

    finalizing the data

--- data.js
+++ data.js
@@ -1,12 +1,13 @@
 const gridData = [
 	{"x":0, "y":0, "data":"potato"},
 	{"x":1, "y":0, "data":"tomato"},
-	{"x":3, "y":4, "data":""},
-	{"x":2, "y":0, "data":"rtero"},
-	{"x":0, "y":1, "data":""},
+	{"x":2, "y":0, "data":"banana"},
+	{"x":0, "y":1, "data":"apple"},
 	{"x":1, "y":1, "data":"bannana"},
-	{"x":2, "y":1, "data":"chekoslovakia"},
-	{"x":0, "y":2, "data":"bannana"},
-	{"x":1, "y":2, "data":""},
-    {"x":2, "y":2, "data":""}
+	{"x":2, "y":1, "data":"pears"},
+	{"x":0, "y":2, "data":"potato"},
+	{"x":1, "y":2, "data":"tomato"},
+    {"x":2, "y":2, "data":"potato"},
+    {"x":1, "y":3, "data":"potato"}
+
 ]
